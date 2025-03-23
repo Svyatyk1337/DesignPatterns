99466f630590c3deecc115d1ee563490bb518175
Sviatoslav Stratii <stratiysviatyk@gmail.com>
2025-03-23 12:11:20 +0200

    Refactored AccountService to follow SRP

--- src/ua/edu/chnu/kkn/solid_violation/srp/AccountService.java
+++ src/ua/edu/chnu/kkn/solid_violation/srp/AccountService.java
@@ -1,85 +1,67 @@
 package ua.edu.chnu.kkn.solid_violation.srp;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.MessageFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import static java.util.stream.Collectors.toCollection;
+import java.util.stream.Collectors;
 
 public class AccountService {
-
-    private static final String STATEMENT_HEADER = "DATE | AMOUNT | BALANCE";
-    private static final String DATE_FORMAT = "dd/MM/yyyy";
-    private static final String AMOUNT_FORMAT = "#.00";
-
     private TransactionRepository transactionRepository;
     private Clock clock;
-    private Console console;
 
-    public AccountService(TransactionRepository transactionRepository, Clock clock, Console console) {
+    public AccountService(TransactionRepository transactionRepository, Clock clock) {
         this.transactionRepository = transactionRepository;
         this.clock = clock;
-        this.console = console;
     }
 
     public void deposit(int amount) {
         transactionRepository.add(transactionWith(amount));
     }
 
-
     public void withdraw(int amount) {
         transactionRepository.add(transactionWith(-amount));
     }
 
-    public void printStatement() {
-        printHeader();
-        printTransactions();
-    }
-
-
-    private void printHeader() {
-        printLine(STATEMENT_HEADER);
-    }
-
-
-    private void printTransactions() {
+    public List<StatementLine> getStatement() {
         List<Transaction> transactions = transactionRepository.all();
-        final AtomicInteger balance = new AtomicInteger(0);
-        transactions.stream()
-                .map(transaction -> statementLine(transaction, balance.addAndGet(transaction.amount())))
-                .collect(toCollection(LinkedList::new))
-                .descendingIterator()
-                .forEachRemaining(this::printLine);
+        AtomicInteger balance = new AtomicInteger(0);
+        return transactions.stream()
+                .map(transaction -> new StatementLine(transaction, balance.addAndGet(transaction.amount())))
+                .collect(Collectors.toList());
     }
-
 
     private Transaction transactionWith(int amount) {
         return new Transaction(clock.today(), amount);
     }
+}
 
+class StatementPrinter {
+    private static final String STATEMENT_HEADER = "DATE | AMOUNT | BALANCE";
 
-    private String statementLine(Transaction transaction, int balance) {
-        return MessageFormat.format("{0} | {1} | {2}", formatDate(transaction.date()), formatNumber(transaction.amount()), formatNumber(balance));
+    private Console console;
+
+    public StatementPrinter(Console console) {
+        this.console = console;
     }
 
-    private String formatDate(LocalDate date) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        return dateFormatter.format(date);
+    public void print(List<StatementLine> statementLines) {
+        console.printLine(STATEMENT_HEADER);
+        statementLines.stream()
+                .map(StatementLine::toString)
+                .forEach(console::printLine);
+    }
+}
+
+class StatementLine {
+    private Transaction transaction;
+    private int balance;
+
+    public StatementLine(Transaction transaction, int balance) {
+        this.transaction = transaction;
+        this.balance = balance;
     }
 
-    private String formatNumber(int amount) {
-        DecimalFormat decimalFormat = new DecimalFormat(AMOUNT_FORMAT, DecimalFormatSymbols.getInstance(Locale.UK));
-        return decimalFormat.format(amount);
-    }
-
-
-    private void printLine(String line) {
-        console.printLine(line);
+    @Override
+    public String toString() {
+        return String.format("%s | %d | %d", transaction.date(), transaction.amount(), balance);
     }
 }
