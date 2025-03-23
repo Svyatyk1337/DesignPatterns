7f045675a825ec89539b3906a2b0260acdadab62
Sviatoslav Stratii <stratiysviatyk@gmail.com>
2025-03-23 12:23:28 +0200

    Refactored code to follow ISP by removing unnecessary methods from the  interface and ensuring each class implements only relevant methods.

--- src/ua/edu/chnu/kkn/solid_violation/isp/Animal.java
+++ src/ua/edu/chnu/kkn/solid_violation/isp/Animal.java
@@ -1,7 +1,5 @@
 package ua.edu.chnu.kkn.solid_violation.isp;
 
 public interface Animal {
-    void fly();
     void run();
-    void bark();
 }
--- src/ua/edu/chnu/kkn/solid_violation/isp/Dog.java
+++ src/ua/edu/chnu/kkn/solid_violation/isp/Dog.java
@@ -2,18 +2,11 @@ package ua.edu.chnu.kkn.solid_violation.isp;
 
 public class Dog implements Animal {
     @Override
-    public void fly() {
-
-    }
-
-    @Override
     public void run() {
         System.out.print("Dog is running");
     }
 
-    @Override
     public void bark() {
         System.out.print("Dog is barking");
     }
 }
-
