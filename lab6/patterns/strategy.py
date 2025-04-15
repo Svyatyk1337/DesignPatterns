a554d8c8590e41830b169a315b25aef12af8d7ad
Sviatoslav Stratii <stratiysviatyk@gmail.com>
2025-04-15 20:24:32 +0300

    Added main function for executing code

--- lab6/patterns/strategy.py
+++ lab6/patterns/strategy.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List, Callable
 
-
-
 def totalValues(values: List[int]) -> int:
     result: int = 0
     for value in values:
@@ -27,5 +25,11 @@ def totalAllValues(values: List[int], filter: Callable[[int], bool]) -> int:
 def isEven(value: int) -> bool:
     return value % 2 == 0
 
-
+if __name__ == "__main__":
+    values: List[int] = [1, 2, 3, 4, 5]
+    print(totalValues(values))
+    print(totalAllEvenValues(values))
+    print(totalAllValues(values, isEven))
+    print(totalAllValues(values, lambda value: value % 2 == 0))
+    
 
